3bfec5f667f91581968f4aa19f9658abc8ee4a39
ps0m <ps0m@tut.by>
2023-01-08 19:15:22 +0300

    feat: add logic for RequireAuth

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,10 +1,20 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { UserContext } from '../../context';
+import { MAIN_PAGE, TODO_PAGE } from '../../router/constants';
 
 interface IRequireAuth {
   children: ReactNode;
 }
 
 const RequireAuth: FC<IRequireAuth> = ({ children }) => {
+  const { user } = useContext(UserContext);
+  const location = useLocation();
+
+  if (!user && location.pathname === TODO_PAGE) {
+    return <Navigate to={MAIN_PAGE} />;
+  }
+
   return <>{children}</>;
 };
 export default RequireAuth;
