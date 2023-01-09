042da69ac386c3dd096298faa5f4428d4e346662
ps0m <ps0m@tut.by>
2023-01-09 12:54:01 +0300

    feat: add logic for NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,4 +1,38 @@
+import GppBadIcon from '@mui/icons-material/GppBad';
+import { Avatar, Button, Stack, Typography } from '@mui/material';
+import { MouseEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MAIN_PAGE } from '../router/constants';
+
 const NotFoundPage = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const goMain = (event: MouseEvent<HTMLButtonElement>) => navigate(MAIN_PAGE, { replace: true });
+  return (
+    <Stack
+      component="section"
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
+      spacing={4}
+    >
+      <Typography variant="h2" component="p">
+        Error 404
+      </Typography>
+      <Avatar
+        sx={{ width: 300, height: 300, backgroundColor: '#fff', boxShadow: '0 0 10px #000' }}
+        alt="logo"
+      >
+        <GppBadIcon sx={{ width: 250, height: 250 }} color="secondary" />
+      </Avatar>
+      <Typography variant="h4" component="p">
+        Page not found
+      </Typography>
+      <Button variant="contained" color="success" size="large" onClick={goMain}>
+        <Typography variant="h3" component="p">
+          To the main page
+        </Typography>
+      </Button>
+    </Stack>
+  );
 };
 export default NotFoundPage;
